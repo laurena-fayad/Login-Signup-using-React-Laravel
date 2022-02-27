9915c115abce8a50132743a1ab15c8d793cdf479
Laurena Fayad <laurenafayad@gmail.com>
2022-02-27 16:27:27 +0200

    Get user data and display it on dashboard

--- react-part/src/pages/Dashboard.js
+++ react-part/src/pages/Dashboard.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const Dashboard = () => {
+
+    const [token, setToken] = useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+
+    useEffect(() => {
+        setToken(localStorage.getItem("token"))
+        axios.get('http://127.0.0.1:8000/api/auth/user-profile',
+        {headers: {"Authorization" : `Bearer ${token}`} })
+        .then(res => {
+            setName(res.data.name);
+            setEmail(res.data.email);
+        }).catch(function (error) {
+            console.log(error);
+        });
+
+    });
+
     return (
         <div>
-            Dashboard
+           <h1>Welcome {name}!</h1> 
+            <h3>Email: {email}</h3>
         </div>
     );
 };
