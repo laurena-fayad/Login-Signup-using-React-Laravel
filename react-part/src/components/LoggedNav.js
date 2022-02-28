0271bfba148ccbf8a80be3e1627c9fe95f883021
Laurena Fayad <laurenafayad@gmail.com>
2022-03-01 00:32:14 +0200

    Fixed logout button styling

--- react-part/src/components/LoggedNav.js
+++ react-part/src/components/LoggedNav.js
@@ -1,53 +1,59 @@
-import React from 'react';
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const LoggedNav = () => {
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
-
-    const logout = () => {
-        localStorage.clear();
-        navigate("/");
-    }
+  const logout = () => {
+    localStorage.clear();
+    navigate("/");
+  };
 
   return (
     <div>
-    <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-      <div className="container-fluid">
-        <Link to="/dashboard" className="navbar-brand">
-          bookmates
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-          aria-controls="navbarCollapse"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <ul className="navbar-nav ms-auto mb-2 mb-md-0">
-            {/* <li className="nav-item">
-              <Link
-                to="/login"
-                className="nav-link active"
-                aria-current="page"
-              >
-                Edit Profile
-              </Link>
-            </li> */}
-            <li className="nav-item">
-              <button onClick={logout}>Logout</button>
-            </li>
-          </ul>
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div className="container-fluid">
+          <Link to="/dashboard" className="navbar-brand">
+            bookmates
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+            aria-controls="navbarCollapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav ms-auto mb-2 mb-md-0">
+              <li className="nav-item">
+                <Link
+                  to="/dashboard"
+                  className="nav-link active"
+                  aria-current="page"
+                >
+                  Edit Profile
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#"
+                  aria-current="page"
+                  onClick={logout}
+                >
+                  Logout
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  </div>
-  )
-}
+      </nav>
+    </div>
+  );
+};
 
-export default LoggedNav
+export default LoggedNav;
