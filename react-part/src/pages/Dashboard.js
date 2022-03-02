92ee7668b7fb4db1a841e0203abf0db35ac4e04c
Laurena Fayad <laurenafayad@gmail.com>
2022-03-02 23:44:27 +0200

    added background image to dashboard

--- react-part/src/pages/Dashboard.js
+++ react-part/src/pages/Dashboard.js
@@ -1,12 +1,14 @@
 import React from "react";
 import LoggedNav from "../components/LoggedNav";
 import Sidebar from "../components/Sidebar";
+import dashboardbg from "../assets/dashboard-bg.jpg"
 
 const Dashboard = () => {
   return (
     <div>
       <LoggedNav />
       <Sidebar/>
+      <img src={dashboardbg} alt="Library Background"/>;      
     </div>
   );
 };
